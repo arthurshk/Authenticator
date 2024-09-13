2bc041a220ef563d21766d7199b29b453d94120c
Arthur <arthurdragon39@gmail.com>
2024-09-12 22:29:43 -0400

    commit

--- authenticator.client/src/App.jsx
+++ authenticator.client/src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
-import ReactDOM from 'react-dom';
+import Login from './Login'
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to My App with Auth0 Authentication</h1>
+            <Login/>
+        </div>
+    );
+};
 
-ReactDOM.render(
-    <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        redirectUri={window.location.origin}
-    >
-        <App />
-    </Auth0Provider>,
-    document.getElementById('root')
-);
-export default app;
+export default App;
--- authenticator.client/src/main.jsx
+++ authenticator.client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import './index.css';
+
+import config from '../auth_config.json';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <Auth0Provider
+            domain={config.domain}
+            clientId={config.clientId}
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    </StrictMode>
+);
